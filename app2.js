e3991900aecd753de5cc17d786dc0128a55632a5
cavinHuang <943866180@qq.com>
2022-10-01 16:41:57 +0800

    fix: 增加文字，并调整视频

--- app2.js
+++ app2.js
@@ -9,7 +9,6 @@ const videoData = require('./videoData.json')
 function getVideoMetaData(filePath) {
   return new Promise((resolve, reject) => {
     ffmpeg.ffprobe(filePath, (err, metadata) => {
-      console.log("🚀 ~ file: app.js ~ line 10 ~ ffmpeg.ffprobe ~ metadata", metadata)
       if (err) {
         return reject(err)
       }
@@ -33,15 +32,11 @@ function getVideoMetaData(filePath) {
   })
 }
 
-function flipVideo(filePath) {
-  const res = execa.commandSync(`ffmpeg.exe -i ${filePath} -vf "vflip" r_1.mp4`)
-  console.log("🚀 ~ file: app.js ~ line 39 ~ flipVideo ~ res", res)
-}
-
-const createFFTask = async (contentVideo) => {
+const createFFTask = async (contentVideo, desc = '') => {
   const videoStart = path.join(__dirname, './start.mp4');
   const cacheDir = path.join(__dirname, './cache/');
   const outputDir = path.join(__dirname, './output/');
+  const font = path.join(__dirname, './font1.ttf');
 
   const { duration, videoHeight, videoWidth } = await getVideoMetaData(contentVideo)
   console.log("🚀 ~ file: app2.js ~ line 47 ~ createFFTask ~ videoWidth", videoWidth)
@@ -70,8 +65,38 @@ const createFFTask = async (contentVideo) => {
   });
   fvideo1.setAudio(false)
   scene1.addChild(fvideo1);
-  scene1.setDuration(10);
-  scene1.setTransition('Fat', 1.5)
+  scene1.setDuration(4);
+  scene1.setTransition('MoveLeft', 1.5)
+
+  if (desc) {
+    const textStr = desc.replace(/\uff0c/g, '\r\n')
+    const text = new FFText({
+      text: textStr,
+      color: '#ffcca9',
+      x: videoWidth / 2,
+      y: 900,
+      font,
+      fontSize: 90
+    })
+    text.alignCenter()
+    text.addEffect('zoomIn', 1, 1)
+    text.setFont(font)
+    text.setStyle({
+      // 'text-shadow': '3px 2px 7px RGB(255,204,169,0.6)',
+      lineHeight: 56,
+      padding: 10,
+      stroke: '#ff0000',
+      strokeThickness: 8,
+      dropShadow: true,
+      dropShadowColor: 'RGB(255,204,169,0.6)',
+      dropShadowBlur: 4,
+      dropShadowAngle: Math.PI / 6,
+      dropShadowDistance: 6,
+      wordWrap: true,
+      wordWrapWidth: 1200,
+    })
+    scene1.addChild(text)
+  }
   creator.addChild(scene1);
 
   const scene2 = new FFScene()
@@ -83,7 +108,8 @@ const createFFTask = async (contentVideo) => {
     height: videoHeight
   })
   contentVideoInstance.setAudio(true)
-  scene2.setDuration(duration)
+  contentVideoInstance.setStartTime(3)
+  scene2.setDuration(duration - 7)
   scene2.addChild(contentVideoInstance)
   creator.addChild(scene2)
 
@@ -118,8 +144,9 @@ const createFFTask = async (contentVideo) => {
 // });
 
 videoData.videoData.forEach(item => {
+  console.log("🚀 ~ file: app2.js ~ line 121 ~ item", item)
   FFCreatorCenter.addTask(async () => {
-    const creator = await createFFTask(item.path)
+    const creator = await createFFTask(item.path, item.desc)
     return creator
   })
 })
